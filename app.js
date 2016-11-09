2b9e0f7937c8a5f7f01ceff0379882f45782e560
Bart Miskowiec <miskowbs@rose-hulman.edu>
2016-11-09 00:29:56 -0500

    maybe this is whats wrong

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
