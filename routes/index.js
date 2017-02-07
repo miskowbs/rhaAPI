72eea5945b699d05e881e8084c9046c93700bbe8
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-02-07 15:55:55 -0500

    maybe put will work?

--- routes/index.js
+++ routes/index.js
@@ -701,7 +701,7 @@ router.put('/api/v1/payment/:id', (req, res, next) => {
 
     client.query(firstQuery, colValues);
 
-    const query = client.query('SELECT * FROM expenses WHERE proposal_id = $1 and CM = $2 and receiver = $3 and amountUsed = $4 and description = $5 and accountCode = $6', [reqJson.proposal_id, reqJson.CM, reqJson.receiver, reqJson.amountUsed, reqJson.description, reqJson.accountCode]) ;
+    const query = client.query('SELECT * FROM expenses WHERE proposal_id = $1 and CM = $2 and receiver = $3 and amountUsed = $4 and description = $5 and accountCode = $6', [proposal_id, CM, receiver, amountUsed, description, accountCode]) ;
 
     query.on('row', (row) => {
       results.push(row);
