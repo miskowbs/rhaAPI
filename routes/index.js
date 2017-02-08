627cccf51062a3ab26b6c1894edce55a0386a9f7
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-02-08 16:38:52 -0500

    removed one check

--- routes/index.js
+++ routes/index.js
@@ -932,7 +932,7 @@ router.post('/api/v1/floorExpense', (req, res, next) => {
     client.query('INSERT INTO floorExpenses(floor_id, event_description, amount, turned_in_date, processed_date) VALUES ($1, $2, $3, $4, $5);',
       [data.floor_id, data.event_description, data.amount, data.turned_in_date, data.processed_date]);
 
-    const query = client.query('SELECT * FROM floorExpenses, floorMoney WHERE floorExpenses.event_description = $1 and floorExpenses.amount = $2 and floorExpenses.turned_in_date = $3 and floorExpenses.processed_date = $4 and floorMoney.hall_and_floor = $5 and floorMoney.floorMoney_id = floorExpenses.floor_id', [data.event_description, data.amount, data.turned_in_date, data.processed_date, data.floor_id] )
+    const query = client.query('SELECT * FROM floorExpenses, floorMoney WHERE floorExpenses.event_description = $1 and floorExpenses.amount = $2 and floorExpenses.turned_in_date = $3 and floorExpenses.processed_date = $4 and floorMoney.floorMoney_id = floorExpenses.floor_id', [data.event_description, data.amount, data.turned_in_date, data.processed_date] )
 
     console.log("query is: ");
     console.log(query);
