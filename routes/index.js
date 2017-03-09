f7845e8b036cac398cab68ad311a08ccbba9a500
Bart Miskowiec <miskowbs@rose-hulman.edu>
2017-03-09 16:00:48 -0500

    addtl logging

--- routes/index.js
+++ routes/index.js
@@ -1323,7 +1323,7 @@ router.put('/api/v1/infoText/:id', (req, res, next) => {
       console.log(err);
       return res.status(500).json({success: false, data: "You broke it so hard it stopped =("});
     }
-
+    console.log(req.body);
     var firstQuery = createUpdateQuery(id, 'info_text_id', req.body, 'infoText'); 
     console.log(firstQuery);
     var colValues = [];
